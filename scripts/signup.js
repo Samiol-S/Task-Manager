5d18abb194e359dfdcd96d47d76fd3587f5cd666
Sam.S <samlisa44@gmail.com>
2025-05-29 18:51:23 +0300

    Finished the sign up page

---
+++ scripts/signup.js
@@ -0,0 +1,67 @@
+
+const upName = document.querySelector('.signup-name-input');
+const upMail = document.querySelector('.signup-email-input');
+const upPass = document.querySelector('.signup-password-input');
+
+function signUp() {
+  const name = upName.value;
+  const email = upMail.value;
+  const password = upPass.value;
+
+  const signUpPromise = fetch('https://sam-task-manager.srv656652.hstgr.cloud/auth/signup', {
+    method:"POST",
+    body:JSON.stringify({
+      name: name,
+      email: email,
+      password: password
+    }),
+    headers: {"Content-Type": "application/json"}
+  });
+  signUpPromise.then((response) => {
+    return response.json();
+  }).then((json) => {
+    if (json.status === true) {
+      alert('Account Created Successfully, Please Sign In to your account!')
+    }
+    else {
+      alert('there has been an error creating your account :(')
+    }
+  })
+}
+
+const SignUpButton = document.querySelector('.sign-up-button');
+SignUpButton.addEventListener('click', signUp);
+
+
+
+
+
+
+
+// Section of making / deleting and getting cookies.
+
+function makeCookie (name, value, daysToLive) {
+  let date = new Date();
+  date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + date.toUTCString();
+  document.cookie=`${name}=${value}; ${expires}; path=/`
+}
+function getCookieByName(name) {
+  const cookieString = document.cookie;
+  const cookies = cookieString.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith(name + '=')) {
+          return cookie.substring(name.length + 1);
+      }
+  }
+  return null;
+}
+
+
+
+
+
+console.log(document.cookie);
+
+
